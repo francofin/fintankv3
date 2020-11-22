d89227366fcb28b21f8145952ee0f6bf6dbe09f9
fintank <finapptank@gmail.com>
2020-11-22 02:48:10 -0500

    added portfolio optimization functionality

--- investorprofile/views.py
+++ investorprofile/views.py
@@ -77,10 +77,86 @@ def logout(request):
 
 @login_required(login_url='login')
 def dashboard(request):
+    import pandas as pd
+    import requests
+    import json
+
+    def make_portfolio_df(portfolio, tickers):
+        data_df =  pd.DataFrame(data = portfolio[0])[['date','adjClose']].rename(columns={'adjClose':tickers[0]}).set_index('date')
+        i = 1
+        while i < len(portfolio):
+            data = pd.DataFrame(portfolio[i])[['date','adjClose']].set_index('date')
+            data_df[tickers[i]] = data['adjClose']
+            i +=1
+            return data_df
+
+    def get_prices(data):
+        i = 0
+        output = []
+        while i < len(data):
+            batch = json.loads(requests.get(f' https://fmpcloud.io/api/v3/historical-price-full/'+data[i]+'?from=2018-03-12&to=2019-03-12&apikey=3da6aaea4ffa4232c7ada6b09e15af62').content)
+            output.append(batch['historical'])
+            i+=1
+        return output
+
+    def price_normalization(dataframe):
+        norm_price = (dataframe / dataframe.shift(1))
+        return norm_price.dropna()
+
+    def log_return(dataframe):
+        log_r = np.log(dataframe/dataframe.shift(1))
+        return log_r
+
+    def cor_table(dataframe):
+        ret_table = log_return(dataframe)
+
+        return ret_table.corr()
+
+    def cov_table(dataframe):
+        ret_table = log_return(dataframe)
+        return ret_table.cov()
+
+    def monte_carlo_sim(r_table_mean, r_cov, a_list, num_trials=5000):
+        all_weights = np.zeros((num_trials, len(a_list)))
+        ret_arr = np.zeros(num_trials)
+        vol_arr = np.zeros(num_trials)
+        sharpe_arr = np.zeros(num_trials)
+
+        for trial in range(num_trials):
+            weights = np.array(np.random.random(len(a_list)))
+            weights = weights / np.sum(weights)
+            all_weights[trial, :] = weights
+            ret_arr[trial] = np.sum((r_table_mean * weights) *252)
+            vol_arr[trial] = np.sqrt(np.dot(weights.T, np.dot(r_cov, weights)))
+            sharpe_arr[trial] = ret_arr[trial] / vol_arr[trial]
+
+        max_sharpe = sharpe_arr.max()
+        max_sr_ret = ret_arr[sharpe_arr.argmax()]
+        best_vol = vol_arr[sharpe_arr.argmax()]
+        best_weights = np.round(all_weights[sharpe_arr.argmax(),:], 4)
+
+        return ret_arr, vol_arr, sharpe_arr, format(max_sharpe, ".2f"), str(format(max_sr_ret *100, ".2f")) +"%", str(format(best_vol*100, ".2f")) +"%", best_weights
+
+
     user_stocks = Portfolio.objects.all().filter(user_id=request.user.id)
-    # user_lifestyle = LifestyleDashboard.object.all().filter(user_id=request.user.id)
+    user_portfolio = []
+    #for the price data
+    data_list = []
+    for stock in user_stocks:
+        user_portfolio.append(str(stock))
+
+    batch = json.loads(requests.get(f' https://fmpcloud.io/api/v3/quote/'+','.join(user_portfolio)+'?apikey=3da6aaea4ffa4232c7ada6b09e15af62').content)
+    prices = get_prices(user_portfolio)
+    prices_df = make_portfolio_df(prices, user_portfolio)
+    log_returns = log_return(prices_df)
+    mean_returns = log_returns.mean()
+    annualized_returns = mean_returns.mean()* 252
+    cov_df = cov_table(prices_df) * 252
+    return_array, volatility_array, sharpe_array, max_sharpe_ratio, max_sharpe_return, best_volatility, port_weights = monte_carlo_sim(mean_returns, cov_df, user_portfolio)
 
     stocks = {
-        'user_stocks': user_stocks
+        'user_stocks': user_stocks,
+        'user_portfolio':user_portfolio,
+        'batch':batch
     }
     return render(request, 'pages/dashboard.html', stocks)
